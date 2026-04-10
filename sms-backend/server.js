15bb6ebf2d8d9c423590b27e58e92dbf1bdf573c
RAVI5216 <ravikumar117284@gmail.com>
2026-04-10 23:08:06 +0530

    fixed mongo atlas connection

--- sms-backend/server.js
+++ sms-backend/server.js
@@ -10,7 +10,7 @@ app.use(cors());
 app.use(express.json());
 
 // MongoDB connection
-mongoose.connect("mongodb://127.0.0.1:27017/smsDB")
+mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log("MongoDB Connected"))
   .catch(err => console.log(err));
 
